28dc7bd6830419211f81778f540e351cddfd0bda
Nat <126525493+NMyers17@users.noreply.github.com>
2023-04-19 15:14:25 +0100

    Add files via upload

--- zalgon-custom-stuff/scripts/modules/sheetEdits.mjs
+++ zalgon-custom-stuff/scripts/modules/sheetEdits.mjs
@@ -15,6 +15,8 @@ export class SheetEdits {
   async render(html) {
     if (this.sheet.document.type === "character") this._renameRestLabels(html);
     if (this.sheet.document.type === "character") this._removeResources(html);
+    if (this.sheet.document.type === "character") this._createNewDay();
+    if (this.sheet.document.type === "character") this._createInspirationToggle();
   }
   
   /** Rename Rest Labels */
@@ -31,4 +33,50 @@ export class SheetEdits {
     if (resources) resources.remove();
   }
 
+   /** Create 'New Day' button after Short and Long rest buttons.*/
+   _createNewDay() {
+    const lr = this.html[0].querySelector(".rest.long-rest");
+    const div = document.createElement("DIV");
+    div.innerHTML = "<a class='rest new-day' data-tooltip='DND5E.NewDay'>Day</a>";
+    div.querySelector(".new-day").addEventListener("click", this._onClickNewDay.bind(this.sheet));
+    lr.after(div.firstChild);
+  }
+
+    /**
+   * Roll limited uses recharge of all items that recharge on a new day.
+   * @param {PointerEvent} event      The initiating click event.
+   * @returns {Item5e[]}              The array of updated items.
+   */
+    async _onClickNewDay(event) {
+      const conf = await Dialog.confirm({
+        title: "New Day",
+        content: "Would you like to recharge all items that regain charges on a new day?",
+        options: {id: `${this.document.uuid.replaceAll(".", "-")}-new-day-confirm`}
+      });
+      if (!conf) return;
+      const updates = await this.document._getRestItemUsesRecovery({
+        recoverShortRestUses: false,
+        recoverLongRestUses: false,
+        recoverDailyUses: true,
+        rolls: []
+      });
+      return this.document.updateEmbeddedDocuments("Item", updates);
+    }
+
+    /** Make 'Inspiration' a toggle. */
+  _createInspirationToggle() {
+    const insp = this.html[0].querySelector(".inspiration h4");
+    insp.classList.add("rollable");
+    insp.dataset.action = "inspiration";
+    insp.addEventListener("click", this._onClickInspiration.bind(this.sheet));
+  }
+
+  /**
+   * Toggle inspiration on or off when clicking the 'label'.
+   * @param {PointerEvent} event      The initiating click event.
+   * @returns {Actor}                 The updated actor.
+   */
+  async _onClickInspiration(event) {
+    return this.document.update({"system.attributes.inspiration": !this.document.system.attributes.inspiration});
+  }
 }
