7e5e0ec05b8d8c2573ddad3b515c65524c345e1e
Nat <126525493+NMyers17@users.noreply.github.com>
2023-05-01 00:54:13 +0100

    Add files via upload

--- zalgon-custom-stuff/scripts/const.mjs
+++ zalgon-custom-stuff/scripts/const.mjs
@@ -1,1 +1,33 @@
 export const MODULE = "zalgon-custom-stuff"
+export const DEPEND = {
+    BAB: "babonus",
+    CN: "concentrationnotifier",
+    EM: "effectmacro",
+    ET: "effective-transferral",
+    JB2A: "jb2a_patreon",
+    RG: "rollgroups",
+    SEQ: "sequencer",
+    VAE: "visual-active-effects",
+    WG: "warpgate"
+  };
+  export const WORLD_DEFAULTS = {
+    addConditions: true,
+    replaceTokenConditions: true
+  }
+  export const COLOR_DEFAULTS = {
+    itemAttuned: "#21c050",
+    itemNotAttuned: "#c2c2c2",
+    showLimitedUses: true,
+    showSpellSlots: true,
+    spellAlwaysPrepared: "#ff0004",
+    spellNotPrepared: "#c2c2c2",
+    spellPrepared: "#0000ff",
+    usesUnexpended: "#ff2e2e"
+  };
+  export const RARITY_DEFAULTS = {
+    uncommon: "#008000",
+    rare: "#0000ff",
+    veryRare: "#800080",
+    legendary: "#ffa500",
+    artifact: "#d2691e"
+  };
--- zalgon-custom-stuff/setup.mjs
+++ zalgon-custom-stuff/setup.mjs
@@ -1,5 +1,22 @@
-import {_performSheetEdits} from "./scripts/modules/sheetEdits.mjs";
+import {registerSettings} from "./scripts/settings.mjs";
+import {refreshColors, _performSheetEdits} from "./scripts/modules/sheetEdits.mjs";
 import {EXHAUSTION} from "./scripts/modules/zalgon_functions.mjs";
+import {DEPEND, MODULE} from "./scripts/const.mjs";
+
+Hooks.once("init", registerSettings);
+Hooks.once("init", () => {
+    game.settings.register("zalgon-custom-stuff", "rules", {
+        name: "Rules",
+        hint: "Custom rules",
+        scope: "world",
+        config: false,
+        type: Object,
+        default: {}
+    });
+});
+Hooks.once("ready", refreshColors);
+
+
 
 Hooks.on("renderActorSheet", _performSheetEdits);
 Hooks.on("dnd5e.restCompleted", EXHAUSTION._longRestExhaustionReduction);
