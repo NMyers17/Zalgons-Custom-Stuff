e2ff7dd75140fdbcb667f8c82570811dc0eb0d77
Nat <126525493+NMyers17@users.noreply.github.com>
2023-05-03 12:53:32 +0100

    Add files via upload

--- zalgon-custom-stuff/setup.mjs
+++ zalgon-custom-stuff/setup.mjs
@@ -1,22 +1,8 @@
-import {registerSettings} from "./scripts/settings.mjs";
-import {refreshColors, _performSheetEdits} from "./scripts/modules/sheetEdits.mjs";
+import {_performSheetEdits} from "./scripts/modules/sheetEdits.mjs";
+import {_activeEffectConfig} from "./scripts/modules/gameChanges.mjs";
 import {EXHAUSTION} from "./scripts/modules/zalgon_functions.mjs";
 import {DEPEND, MODULE} from "./scripts/const.mjs";
 
-Hooks.once("init", registerSettings);
-Hooks.once("init", () => {
-    game.settings.register("zalgon-custom-stuff", "rules", {
-        name: "Rules",
-        hint: "Custom rules",
-        scope: "world",
-        config: false,
-        type: Object,
-        default: {}
-    });
-});
-Hooks.once("ready", refreshColors);
-
-
-
 Hooks.on("renderActorSheet", _performSheetEdits);
 Hooks.on("dnd5e.restCompleted", EXHAUSTION._longRestExhaustionReduction);
+Hooks.on("renderActiveEffectConfig", _activeEffectConfig);
