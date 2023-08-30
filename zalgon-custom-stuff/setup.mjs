d73e3166dd31cabe5bffde04443738a024f1870e
Nat <126525493+NMyers17@users.noreply.github.com>
2023-08-30 15:28:34 +0100

    Delete zalgon-custom-stuff directory

--- zalgon-custom-stuff/setup.mjs
+++
@@ -1,7 +0,0 @@
-import {_performSheetEdits} from "./scripts/modules/sheetEdits.mjs";
-import {_activeEffectConfig} from "./scripts/modules/gameChanges.mjs";
-import {EXHAUSTION} from "./scripts/modules/zalgon_functions.mjs";
-
-Hooks.on("renderActorSheet", _performSheetEdits);
-Hooks.on("dnd5e.restCompleted", EXHAUSTION._longRestExhaustionReduction);
-Hooks.on("renderActiveEffectConfig", _activeEffectConfig);
